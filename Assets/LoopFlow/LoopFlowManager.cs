2e1e14e4257aedd6f93d9b05246fd3c345f341ce
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-04-16 00:06:20 +0900

    [Update] Decoupling LoopFlowManager

---
+++ Assets/LoopFlow/LoopFlowManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Custom;
+using Isometric.Interface;
+
+public class LoopFlowManager : LoopFlow
+{
+    protected LoopFlow requestedLoopFlow;
+    protected LoopFlow currentLoopFlow;
+
+    protected float transitTime;
+    protected bool transiting;
+
+    public LoopFlowManager() : base()
+    {
+        currentLoopFlow = null;
+    }
+
+    public override void Update(float deltaTime)
+    {
+        transitTime = transitTime - deltaTime;
+
+        if (transitTime <= 0f)
+            SwitchLoopFlow(requestedLoopFlow);
+
+        base.Update(deltaTime);
+    }
+
+    public virtual void SwitchLoopFlow(LoopFlow newLoopFlow)
+    {
+        if (newLoopFlow == null || currentLoopFlow == newLoopFlow)
+            return;
+
+        if (currentLoopFlow != null)
+            currentLoopFlow.Terminate();
+
+        currentLoopFlow = newLoopFlow;
+        AddSubLoopFlow(currentLoopFlow);
+
+        transiting = false;
+    }
+
+    public virtual void RequestSwitchLoopFlow(LoopFlow newLoopFlow, float fadeOutSeconds = 0.5f)
+    {
+        if (transiting)
+            return;
+
+        requestedLoopFlow = newLoopFlow;
+
+        transiting = true;
+        transitTime = fadeOutSeconds;
+    }
+}
---
+++ Assets/Project-Isometric/LoopFlow/IsometricLoopFlowManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Custom;
+using Isometric.Interface;
+
+public class IsometricLoopFlowManager : LoopFlowManager
+{
+    private FSprite fadeSprite;
+    private FLabel fadeLabel;
+
+    private float transitFactor;
+
+    public IsometricLoopFlowManager(IsometricMain main) : base()
+    {
+        fadeSprite = new FSprite("pixel");
+        fadeSprite.scaleX = MenuFlow.screenWidth;
+        fadeSprite.scaleY = MenuFlow.screenHeight;
+        fadeSprite.color = Color.black;
+
+        fadeLabel = new FLabel("font", "Loading...");
+        fadeLabel.alignment = FLabelAlignment.Right;
+        fadeLabel.SetPosition(MenuFlow.rightDown + new Vector2(-10f, 10f));
+    }
+
+    public override void RawUpdate(float deltaTime)
+    {
+        base.RawUpdate(Mathf.Min(deltaTime, 0.05f));
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            HandleExecuteEscape();
+    }
+
+    public override void Update(float deltaTime)
+    {
+        transitFactor = Mathf.Clamp01(transitFactor + (transiting ? -deltaTime : deltaTime) / 0.5f);
+
+        fadeSprite.alpha = CustomMath.Curve(1f - transitFactor, 1f);
+
+        base.Update(deltaTime);
+    }
+
+    public override void RequestSwitchLoopFlow(LoopFlow newLoopFlow, float fadeOutSeconds = 0.5f)
+    {
+        base.RequestSwitchLoopFlow(newLoopFlow, fadeOutSeconds);
+
+        Futile.stage.AddChild(fadeSprite);
+        Futile.stage.AddChild(fadeLabel);
+    }
+
+    public override void SwitchLoopFlow(LoopFlow newLoopFlow)
+    {
+        base.SwitchLoopFlow(newLoopFlow);
+
+        Futile.stage.AddChild(fadeSprite);
+        Futile.stage.RemoveChild(fadeLabel);
+    }
+}
