191db763e6b5200e9a72fc9f0de6275455ba72ed
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-06-06 23:55:08 +0900

    [Add] Tutorial NPC

---
+++ Assets/Project-Isometric/IsometricGame/Entity/TutorialNPC.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TutorialNPC : EntityCreature
+{
+    private const string String = "W A S D : Move the character\nSpace : Jump the character\nQ, E : Move the camera\nEsc : Exit the game";
+
+    public TutorialNPC() : base(1f, 2f, 100f)
+    {
+
+    }
+
+    public override void OnSpawn(Chunk chunk, Vector3 position)
+    {
+        base.OnSpawn(chunk, position);
+
+        HearAdvice();
+    }
+
+    public void HearAdvice()
+    {
+        world.cameraHUD.Speech(this, String);
+    }
+}
